078298a7bd3c7cd6932f2fd2bf33a467cf9288b2
garay88 <emmanuel.garay@udea.edu.co>
2021-04-10 14:38:49 -0500

    falta analizar lo de los punteros

--- Exer_1/main.cpp
+++ Exer_1/main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
-    return 0;
+void fun_a(int *px,int *py);
+void fun_b(int a[],int tam);
+
+int main(){
+    int array[10] = {10, 20, 30, 40, 50, 90, 97, 98, 99, 101};
+    fun_b(array, 10);
+}
+
+void fun_a(int *px,int *py){
+    int tmp = *px;
+    *px = *py;
+    *py = tmp;
+}
+
+void fun_b(int a[],int tam){
+    int f=0, l;
+    int *b = a;
+    for (l = tam -1; f < 1; f++, l--){
+        fun_a(&b[f], &b[l]);
+    }
 }
